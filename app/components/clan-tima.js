452db191874ad8bac92ba96b1fe837f606f4661d
Eptick <redzicleon@gmail.com>
2017-04-03 15:52:16 +0200

    Dev

--- app/components/clan-tima.js
+++ app/components/clan-tima.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions : {
+        toggleProjekti(id) {
+            var selektor = "#clan-projekti-"+id; 
+            if(Ember.$(selektor).hasClass("active")){
+                Ember.$(selektor).removeClass("active");
+                Ember.$(selektor).fadeOut();     
+            } else {
+                Ember.$(selektor).addClass("active");
+                Ember.$(selektor).fadeIn(); 
+            }
+        }
+    }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -9,7 +9,7 @@ export default Ember.Route.extend({
       godina: 'Treća godina preddiplomskog studija',
       stanovanje: 'Varaždin/Sisak',
       fokus: 'FullStack',
-      image: 'http://placehold.it/250x400',
+      image: 'img/redzic.jpg',
       links: [
         {
           link: 'https://github.com/Eptick',
@@ -18,7 +18,24 @@ export default Ember.Route.extend({
         {
           link: 'http://leon-redzic.from.hr',
           class: 'desktop'
-        }]
+        }],
+        projekti : [
+          {
+            ime: "Tray Music App",
+            img: "img/projekti/redzic/musicapp.png",
+            opis: "Aplikacija koja omogućava preuzimanje youtube sadržaja u mp3 formatu te ujedino služi kao i music player"
+          },
+          {
+            ime: "SQL Query helper",
+            img: "img/projekti/redzic/baze.png",
+            opis: "Aplikacija se spaja na MySQL bazu podataka te pomaže u pisajnu jednostavnih SQL upita preko query buildera"
+          },
+          {
+            ime: "Green website",
+            img: "img/projekti/redzic/green.png",
+            opis: "Web shop rađen u prestashop razvojnom okviru"
+          }
+        ]
     },
     {
       id: '2',
@@ -26,35 +43,51 @@ export default Ember.Route.extend({
       fakultet: 'Fakultet Organizacije i Informatike',
       godina: 'Treća godina preddiplomskog studija',
       stanovanje: 'Čakovec',
-      fokus: 'Backend',
-      image: 'http://placehold.it/250x400',
+      fokus: 'Baze podataka',
+      image: 'img/solja.png',
       links: [
         {
-          link: 'https://github.com/Eptick',
+          link: 'https://github.com/jursolja',
           class: 'github'
-        },
-        {
-          link: 'http://leon-redzic.from.hr',
-          class: 'desktop'
-        }]
+        }],
+        projekti : [
+          {
+            ime: "MQtt Chat Aplikacija",
+            img: "img/projekti/solja/mqtt.png",
+            opis: "Ovo je chat aplikacija rađena u Window form tehnologiji. Aplikacija koristi MQtt okvir. Rađena je kao projekt za kolegij Mreže Računala 2"
+          },
+          {
+            ime: "Rezervacija hotela",
+            img: "img/projekti/solja/baze.png",
+            opis: "Rađen je Era model te pripadajuća baza podataka i web aplikacija. Aplikacija služi za rezerviranje soba u hotelu. Projekt je rađen u sklopu kolegija Baze podataka 2 te u duhu kolegija korištene su sve mogućnosti baze i alata za razvoj."
+          }
+        ]
     },
     {
       id: '3',
       ime: 'Marko Škvorc',
       fakultet: 'Fakultet Organizacije i Informatike',
       godina: 'Treća godina preddiplomskog studija',
-      stanovanje: 'Pripizdina neka',
-      fokus: 'Baze podataka',
-      image: 'http://placehold.it/250x400',
+      stanovanje: 'Poture',
+      fokus: 'BackEnd',
+      image: 'img/skvorc.jpg',
       links: [
         {
-          link: 'https://github.com/Eptick',
+          link: 'https://github.com/marskvorc',
           class: 'github'
-        },
-        {
-          link: 'http://leon-redzic.from.hr',
-          class: 'desktop'
-        }]
+        }],
+        projekti : [
+          {
+            ime: "Složi PC",
+            img: "img/projekti/skvorc/slozi.png",
+            opis: "Aplikacija ima bazu podataka koja sadrži različite komponente računala te po zadanim kriterijima predlaže knfiguraciju za vaše uvijete."
+          },
+          {
+            ime: "MQtt Chat Aplikacija",
+            img: "img/projekti/solja/mqtt.png",
+            opis: "Ovo je chat aplikacija rađena u Window form tehnologiji. Aplikacija koristi MQtt okvir. Rađena je kao projekt za kolegij Mreže Računala 2"
+          }
+        ]
     }];
   }
 });
